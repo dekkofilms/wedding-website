8e86cba56e4f3885eb2be379e7e5ae285945e6e2
Taylor King <dekkofilms@gmail.com>
2017-08-19 11:02:32 -0500

    added an RSVP form that syncs up to our google form

--- src/components/RSVP/Rsvp.js
+++ src/components/RSVP/Rsvp.js
@@ -1,20 +1,92 @@
 import React, { Component } from 'react';
-import { Row, Col, FormGroup, ControlLabel, FormControl, Radio, Button } from 'react-bootstrap';
+import { Row, Col, FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap';
+import { RadioGroup, RadioButton } from 'react-radio-buttons';
 
 class Rsvp extends Component {
   constructor(props, context) {
     super(props, context);
 
-    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.state = {
+      attending: 'Yes'
+    }
+
+    this.handleRadioChange = this.handleRadioChange.bind(this);
   }
 
-  onFormSubmit(e) {
-    e.preventDefault()
+  handleRadioChange(attending) {
+    this.setState({ attending });
   }
 
   render() {
     return (
-      <div className="main-container">
+      <div className="rsvp-wrapper">
+        <form action="https://docs.google.com/forms/d/e/1FAIpQLSdgY2K-cxWk4KEO9Nd6TkhZjLqlA0qbKNbw2hCpkImLCCqyqQ/formResponse">
+          <Row className="form-row">
+            <Col md={8} mdOffset={2} xs={12}>
+              <FormGroup controlId="formBasicText">
+                <ControlLabel className="form-label">What is your name?</ControlLabel>
+                <FormControl
+                  className="short-input-field"
+                  name="entry.794282248"
+                  type="text"
+                  autoFocus={true}
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+          <Row className="form-row">
+            <Col md={8} mdOffset={2} xs={12}>
+              <FormGroup controlId="formBasicText">
+                <ControlLabel className="form-label">What is your email?</ControlLabel>
+                <FormControl
+                  className="short-input-field"
+                  name="entry.1982348805"
+                  type="text"
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+          <Row className="form-row">
+            <Col md={8} mdOffset={2} xs={12}>
+              <FormGroup>
+                <ControlLabel className="form-label">Will you be attending?</ControlLabel>
+                <div style={{paddingLeft: 42, paddingRight: 42}}>
+                  <RadioGroup onChange={this.handleRadioChange} horizontal>
+                    <RadioButton rootColor="#E0E0E0" pointColor="#212121" value="Yes">
+                      Yes
+                    </RadioButton>
+                    <RadioButton rootColor="#E0E0E0" pointColor="#212121" value="No">
+                      No
+                    </RadioButton>
+                    <input type="hidden" name="entry.1106972459" value={this.state.attending} />
+                  </RadioGroup>
+                </div>
+              </FormGroup>
+            </Col>
+          </Row>
+          <Row className="form-row">
+            <Col md={8} mdOffset={2} xs={12}>
+              <FormGroup controlId="formControlsSelect">
+                <ControlLabel className="form-label">How many in party?</ControlLabel>
+                <FormControl className="short-input-field styled-select" name="entry.1078287513" componentClass="select" placeholder="select">
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                  <option value="4">Four</option>
+                  <option value="5">Five</option>
+                  <option value="6">Six</option>
+                </FormControl>
+              </FormGroup>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={8} mdOffset={2} xs={12}>
+              <Button bsClass="custom-button" className="pull-right" type="submit">
+                Submit
+              </Button>
+            </Col>
+          </Row>
+        </form>
       </div>
     );
   }
@@ -22,73 +94,20 @@ class Rsvp extends Component {
 
 export default Rsvp;
 
-// <form action="https://docs.google.com/forms/d/e/1FAIpQLSdgY2K-cxWk4KEO9Nd6TkhZjLqlA0qbKNbw2hCpkImLCCqyqQ/formResponse" onSubmit={this.onFormSubmit}>
-//   <Row>
-//     <Col md={8} mdOffset={2} xs={12}>
-//       <FormGroup controlId="formBasicText">
-//         <ControlLabel>What is your name?</ControlLabel>
-//         <FormControl
-//           name="entry.794282248"
-//           type="text"
-//           placeholder="Enter text"
-//         />
-//       </FormGroup>
+// <FormGroup>
+//   <ControlLabel className="form-label">Will you be attending?</ControlLabel>
+//   <Row style={{paddingLeft: 42}}>
+//     <Col xs={12}>
+//       <Radio name="entry.1106972459" value="Yes" inline>
+//         Yes
+//       </Radio>
 //     </Col>
 //   </Row>
-//   <Row>
-//     <Col md={8} mdOffset={2} xs={12}>
-//       <FormGroup controlId="formBasicText">
-//         <ControlLabel>What is your email address?</ControlLabel>
-//         <FormControl
-//           name="entry.1982348805"
-//           type="text"
-//           placeholder="Enter text"
-//         />
-//       </FormGroup>
+//   <Row style={{paddingLeft: 42}}>
+//     <Col xs={12}>
+//       <Radio name="entry.1106972459" value="No" inline>
+//         No
+//       </Radio>
 //     </Col>
 //   </Row>
-//   <Row>
-//     <Col md={8} mdOffset={2} xs={12}>
-//       <FormGroup>
-//         <ControlLabel>Will you be attending?</ControlLabel>
-//         <Row style={{paddingLeft: 10}}>
-//           <Col xs={12}>
-//             <Radio name="entry.1106972459" value="Yes" inline>
-//               Yes
-//             </Radio>
-//           </Col>
-//         </Row>
-//         <Row style={{paddingLeft: 10}}>
-//           <Col xs={12}>
-//             <Radio name="entry.1106972459" value="No" inline>
-//               No
-//             </Radio>
-//           </Col>
-//         </Row>
-//       </FormGroup>
-//     </Col>
-//   </Row>
-//   <Row>
-//     <Col md={8} mdOffset={2} xs={12}>
-//       <FormGroup controlId="formControlsSelect">
-//         <ControlLabel>How many in your party?</ControlLabel>
-//         <FormControl name="entry.1078287513" componentClass="select" placeholder="select">
-//           <option value="select">Choose number...</option>
-//           <option value="1">One</option>
-//           <option value="2">Two</option>
-//           <option value="3">Three</option>
-//           <option value="4">Four</option>
-//           <option value="5">Five</option>
-//           <option value="6">Six</option>
-//         </FormControl>
-//       </FormGroup>
-//     </Col>
-//   </Row>
-//   <Row>
-//     <Col md={8} mdOffset={2} xs={12}>
-//       <Button className="pull-right" type="submit">
-//         Submit
-//       </Button>
-//     </Col>
-//   </Row>
-// </form>
+// </FormGroup>
